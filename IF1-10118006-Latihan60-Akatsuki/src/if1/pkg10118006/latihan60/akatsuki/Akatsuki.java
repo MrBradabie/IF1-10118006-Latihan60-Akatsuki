bcf55526a0ca99ced37605f1c0b7ed52d98c1292
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-11-07 19:44:39 +0700

    Add files via upload

---
+++ IF1-10118006-Latihan60-Akatsuki/src/if1/pkg10118006/latihan60/akatsuki/Akatsuki.java
@@ -0,0 +1,32 @@
+ 
+package if1.pkg10118006.latihan60.akatsuki;
+
+/*
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : IF 1
+ * NIM   : 10118006
+ */
+public class Akatsuki {
+    
+    protected String clothes;
+    protected String goals;
+
+    public Akatsuki(String clothes, String goals) {
+        this.clothes = clothes;
+        this.goals = goals;
+        
+        clothes = "Black Red Cloud";
+        goals = "Gathering all Jinchuriki";
+        
+    }
+
+    public String getClothes() {
+        return clothes;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+    
+}
---
+++ IF1-10118006-Latihan60-Akatsuki/src/if1/pkg10118006/latihan60/akatsuki/IF110118006Latihan60Akatsuki.java
@@ -0,0 +1,42 @@
+ 
+package if1.pkg10118006.latihan60.akatsuki;
+
+/*
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : IF 1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini untuk membuat objek dari anime naruto untuk
+ *                     anggota akatsuki.
+ */
+public class IF110118006Latihan60Akatsuki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String cloth = "Black Red Cloud";
+        String goal = "Gathering all Jinchuriki";
+        
+        Member deidara = new Member("Deidara", "Iwagakure", "Explosive Clay", "S-Rank Missing Ninja", "Explosive Release", "Killed in Battle by Chiyo", true, cloth, goal);
+        Member sasori = new Member("Sasori", "Sunagakure", "Hiruko", "S-Rank Missing Ninja from Sunagakure's Puppet Brigade", "Puppet Mastery", "Killed in Battle by Sakura", true, cloth, goal);
+        Member kisame = new Member("Kisame Hoshigaki", "Kirigakure", "Samehada", "S-Rank Missing Ninja", "Shark-o-mation", "Committed Suicide", true, cloth, goal);
+        Member itachi = new Member("Itachi Uchiha", "Konohagakure", "Kunai", "Anbu Captain Missing Ninja", "Mangekyo Sharingan", "Killed by his own brother", true, cloth, goal);
+        Member orochimaru = new Member("Orochimaru", "Konohagakure", "Snakes", "Konohagakure's Legendary Sannin", "Power of White Snake", "Killed & Revived by Sasuke", false, cloth, goal);
+        
+        System.out.println("Akatsuki Clan");
+        System.out.println("=============");
+        System.out.println();
+        System.out.println("Cloth : " + deidara.getClothes());
+        System.out.println("Goal : " + deidara.getGoals());
+        System.out.println();
+        
+        deidara.displayMember();
+        sasori.displayMember();
+        kisame.displayMember();
+        itachi.displayMember();
+        orochimaru.displayMember();
+    }
+    
+}
